f20816cd1269e694916f8d73e832a8ddcccfcd99
Victor Matyzhonok <victor@matyzhonok.ru>
2019-07-16 21:51:11 +0300

    hometask_2 step 3

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -16,6 +16,7 @@ class User (
     var lastVisit : Date? = null,
     var isOnline : Boolean = false
 ){
+
     constructor(id : String, firstName : String?, lastName : String?) : this(
         id = id,
         firstName = firstName,
@@ -26,10 +27,26 @@ class User (
     constructor(id: String) : this(
         id = id,
         firstName = "John",
-        lastName = "Doe $id"
+        lastName = "Doe"
     )
 
     init {
-        println("I'm are live")
+        println("It`s Alive!!! \n" +
+                "${if(lastName==="Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!" }\n")
+    }
+
+
+    companion object Factory {
+        private var lastId : Int = -1
+        fun makeUser (fullName:String?) : User{
+            lastId++
+
+            val parts : List<String>? = fullName?.split(" ")
+
+            val firstName =parts?.getOrNull(0)
+            val lastName =parts?.getOrNull(1)
+
+            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+        }
     }
 }
