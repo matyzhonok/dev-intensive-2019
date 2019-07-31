be1c8fdfa5318ec06b34c4c7e78574054f2b4ac2
Matyzhonok Victor <victor@matyzhonok.ru>
2019-07-31 23:30:31 +0300

    Правки

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -5,17 +5,11 @@ package ru.skillbranch.devintensive.utils
  */
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?, String?>{
-        var fullNameModifier = fullName?.trim()
-        if ((fullNameModifier == null) or (fullNameModifier == "")) {
-            return null to null
-        }
-        while (fullNameModifier!!.contains("  ")){
-            fullNameModifier = fullNameModifier!!.replace("  "," ")
-        }
-        val parts : List<String>? = fullNameModifier?.split(" ")
 
-        var firstName =parts?.getOrNull(0)
-        var lastName =parts?.getOrNull(1)
+        val parts : List<String>? = fullName?.split(" ")?.filter { !it.isBlank()}
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
 
         return firstName to lastName
     }
